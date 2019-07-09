3e46f757163b9cf3cfeb67ea5ebc5bea26b4db8e
ChrisLHall <mrchrislhall@gmail.com>
2019-07-08 21:44:06 -0700

    Added movement-amplification and world shrinking movement. Pretty happy with them tbh.

---
+++ Assets/ScaleWalk.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ScaleWalk : MonoBehaviour
+{
+    private SteamVR_PlayArea area;
+    public SteamVR_Action_Boolean grabPinch;
+    public float SCALE_UP_MULT;
+    private bool scaled = false;
+    private float savedPosY;
+    void Awake ()
+    {
+        area = GetComponent<SteamVR_PlayArea>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        var cam = SteamVR_Render.Top();
+        Vector3 camLocal = cam.transform.localPosition;
+        //camLocal.y = 0f;
+        if (grabPinch.GetStateDown(SteamVR_Input_Sources.Any) && !scaled)
+        {
+            scaled = true;
+            savedPosY = area.transform.position.y;
+            area.transform.localScale = Vector3.one * SCALE_UP_MULT;
+            area.transform.position -= (SCALE_UP_MULT - 1) * camLocal;
+        }
+        else if (grabPinch.GetStateUp(SteamVR_Input_Sources.Any) && scaled)
+        {
+            scaled = false;
+            area.transform.localScale = Vector3.one;
+            area.transform.position += (SCALE_UP_MULT - 1) * camLocal;
+            Vector3 newPos = area.transform.position;
+            newPos.y = savedPosY;
+            area.transform.position = newPos;
+        }
+    }
+}
---
+++ Assets/ZoomWalk.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class ZoomWalk : MonoBehaviour
+{
+    private SteamVR_PlayArea area;
+    public SteamVR_Action_Boolean grabPinch;
+    public float TRIGGER_WALK_MULTIPLIER;
+    private Vector3 lastLocalPos;
+    void Awake ()
+    {
+        area = GetComponent<SteamVR_PlayArea>();
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        var cam = SteamVR_Render.Top();
+        if (grabPinch.GetState(SteamVR_Input_Sources.Any))
+        {
+            Vector3 posDelta = cam.transform.localPosition - lastLocalPos;
+            posDelta.y = 0f;
+            area.transform.position += posDelta * TRIGGER_WALK_MULTIPLIER;
+        }
+
+        lastLocalPos = cam.transform.localPosition;
+    }
+}
